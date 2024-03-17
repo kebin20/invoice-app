563099aca59d84413508debe3cea5c1c89321b05
kebin20 <ktanzyl@hotmail.com>
2024-03-17 19:47:18 +0900

    build: installed bootstrap and sass

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,22 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/main.scss";`
+      }
+    }
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   overrides: [
     {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
+      files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended']
     }
   ],
   parserOptions: {
