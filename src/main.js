6564bd3e706d72f4fe66b1f6528cf4557d338f19
kebin20 <ktanzyl@hotmail.com>
2024-03-18 22:27:50 +0900

    chore: imported components and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.vue'
-import router from './router'
+import Navbar from './components/Navbar.vue'
 
 const app = createApp(App)
 
+app.component('navbar', Navbar)
+
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
