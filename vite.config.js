d92769743f820e125ba5900e5cfc899633ecbad2
kebin20 <ktanzyl@hotmail.com>
2024-03-19 23:08:08 +0900

    build: installed sass loader

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,24 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'element-plus',
+          resolveStyle: (name) => `element-plus/lib/theme-chalk/${name}.scss`
+          // resolveStyle: (name) => `element-plus/packages/theme-chalk/src/${name}.scss`,
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -16,7 +30,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/styles/main.scss";`
+        additionalData: `@import "@/styles/variables.scss";`
       }
     }
   }
