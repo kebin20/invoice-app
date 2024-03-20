5ccee92722da929b915706ab88d732d7f9261d49
kebin20 <ktanzyl@hotmail.com>
2024-03-20 11:06:19 +0900

    chore: temporarily added background color to html

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import 'bootstrap'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/main.css'
 
 import App from './App.vue'
-import Navbar from './components/Navbar.vue'
 
 const app = createApp(App)
-
-app.component('navbar', Navbar)
 
 app.use(createPinia())
 
