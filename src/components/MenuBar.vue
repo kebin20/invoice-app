d524131f48aec6dfe699a624bce3bdda5703ebcf
kebin20 <ktanzyl@hotmail.com>
2024-03-20 21:12:27 +0900

    style: completed styling for menubar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,24 +1,40 @@
 <template>
-  <section class="d-flex justify-content-between align-items-center m-4">
+  <section class="d-flex justify-content-between align-items-center">
     <div class="d-flex flex-column">
       <h1 class="heading-m text-left">Invoices</h1>
       <p>7 Invoices</p>
     </div>
-    <div class="d-flex gap-4">
-      <div>Filter</div>
-      <button>+ New</button>
+    <div class="d-flex align-items-center gap-3">
+      <div class="filter d-flex align-items-center gap-2">
+        <p class="filter-text">Filter</p>
+        <ArrowDownIcon />
+      </div>
+      <NewInvoiceButton />
     </div>
   </section>
 </template>
 
 <script>
+import NewInvoiceButton from './NewInvoiceButton.vue'
+import ArrowDownIcon from './icons/ArrowDownIcon.vue'
+
 export default {
-  name: 'MenuBar'
+  name: 'MenuBar',
+  components: {
+    NewInvoiceButton,
+    ArrowDownIcon
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 section {
-  font-weight: 400;
+  margin: 2em 1.4em;
+  .filter {
+    font-weight: 700;
+    &-text {
+      margin: 0;
+    }
+  }
 }
 </style>
