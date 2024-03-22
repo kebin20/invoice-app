c8a51eb453efa35973cecef1a55c84aabfc1b04f
kebin20 <ktanzyl@hotmail.com>
2024-03-23 08:53:44 +0900

    feat: created base modal

---
+++ src/components/UI/BaseModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div @click="closeModal"></div>
+  <teleport to="body">
+    <dialog open>
+      <header>
+        <slot name="header">
+          <h2>{{ title }}</h2>
+        </slot>
+      </header>
+      <section>
+        <slot></slot>
+      </section>
+      <menu>
+        <slot name="actions"> </slot>
+      </menu>
+    </dialog>
+  </teleport>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false
+    }
+  },
+  methods: {
+    closeModal() {
+      return this.$emit('close')
+    }
+  }
+}
+</script>
