a82da6d6ae1aa975f9cddf1b9fc5d78c7168939f
kebin20 <ktanzyl@hotmail.com>
2024-03-23 08:53:11 +0900

    feat: enabled output of invoices from json data. Created function to properly format the date and currency

--- src/components/InvoiceCard.vue
+++ src/components/InvoiceCard.vue
@@ -1,17 +1,19 @@
 <template>
-  <li>
+  <li v-for="invoice of invoices" :key="invoice.id">
     <BaseCard>
       <header>
         <div>
-          <h3>{{ InvoiceID }}</h3>
-          <p>{{ recipientName }}</p>
+          <h3>{{ invoice.id }}</h3>
+          <p>{{ invoice.clientName }}</p>
         </div>
         <div>
           <div>
-            <p>{{ DueDate }}</p>
-            <p>{{ TotalPrice }}</p>
+            <p>{{ formatDate(invoice.paymentDue) }}</p>
+            <div v-for="item of invoice.items">
+              <p>{{ formatCurrency(item.total) }}</p>
+            </div>
           </div>
-          <div class="payment-status">- {{ paymentStatus }}</div>
+          <div class="payment-status">- {{ invoice.status }}</div>
         </div>
       </header>
     </BaseCard>
@@ -22,11 +24,19 @@
 export default {
   name: 'InvoiceCard',
   props: {
-    InvoiceID: String,
-    recipientName: String,
-    DueDate: Date,
-    TotalPrice: Number,
-    paymentStatus: String
+    invoices: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString)
+      return date.toLocaleDateString()
+    },
+    formatCurrency(amount) {
+      return amount.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+    }
   }
 }
 </script>
--- src/components/InvoiceList.vue
+++ src/components/InvoiceList.vue
@@ -1,16 +1,22 @@
 <template>
   <ul>
-    <InvoiceCard />
+    <InvoiceCard :invoices="invoices" />
   </ul>
 </template>
 
 <script>
 import InvoiceCard from './InvoiceCard.vue'
+import INVOICE_DATA from '../../data.json'
 
 export default {
   name: 'Invoice List',
   components: {
     InvoiceCard
+  },
+  data() {
+    return {
+      invoices: INVOICE_DATA
+    }
   }
 }
 </script>
