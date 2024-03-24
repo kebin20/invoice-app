132014fa5f95725ef94eadd3fdb09131962153a1
kebin20 <ktanzyl@hotmail.com>
2024-03-24 14:30:46 +0900

    feat: created status indicator component

---
+++ src/components/UI/InvoiceStatus.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="font-weight-bold p-0">
+    <li :class="['invoice-status', invoiceStatusClass(status)]">
+      {{ capitalizeStatus(status) }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'InvoiceCard',
+  props: {
+    status: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    invoiceStatusClass() {
+      return function (status) {
+        let statusClass
+        switch (status) {
+          case 'paid':
+            statusClass = '-paid'
+            break
+          case 'pending':
+            statusClass = '-pending'
+            break
+          case 'draft':
+            statusClass = '-draft'
+            break
+          default:
+            statusClass = ''
+        }
+        return 'invoice-status' + statusClass
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
