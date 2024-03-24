3f14a751e2b28721d3224d8965861035844597e8
kebin20 <ktanzyl@hotmail.com>
2024-03-24 14:31:19 +0900

    style: adjusted styling for menu bar

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -2,7 +2,7 @@
   <section class="d-flex justify-content-between align-items-center">
     <div class="d-flex flex-column">
       <h1 class="heading-m text-left">Invoices</h1>
-      <p>7 Invoices</p>
+      <p class="color-blue m-0">7 Invoices</p>
     </div>
     <div class="d-flex align-items-center gap-3">
       <div class="filter d-flex align-items-center gap-2">
@@ -29,7 +29,7 @@ export default {
 
 <style lang="scss" scoped>
 section {
-  margin: 2em 1.4em;
+  margin: 2em 1.5em;
   .filter {
     font-weight: 700;
     &-text {
