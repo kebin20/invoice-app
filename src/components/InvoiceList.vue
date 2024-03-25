5895b3a162932c03f524ba02658eb7e4b4db5f77
kebin20 <ktanzyl@hotmail.com>
2024-03-25 21:59:01 +0900

    feat: created screen to show no invoices available

--- src/components/InvoiceList.vue
+++ src/components/InvoiceList.vue
@@ -1,8 +1,15 @@
 <template>
   <section class="d-flex flex-column align-items-center">
-    <ul class="p-0 mx-5">
+    <ul class="p-0 mx-5" v-if="invoicesAvailable">
       <InvoiceCard :invoices="invoices" />
     </ul>
+    <div class="d-flex flex-column align-items-center text-center mt-5">
+      <img src="../assets/illustration-empty.svg" alt="No invoices" />
+      <div class="p-5">
+        <h1>There's nothing here</h1>
+        <p class="mt-4 p-2">Create an invoice by clicking the New button and get started</p>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -19,6 +26,17 @@ export default {
     return {
       invoices: INVOICE_DATA
     }
+  },
+  computed: {
+    invoicesAvailable() {
+      return this.invoices.length > 0
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h1 {
+  font-weight: 700;
+}
+</style>
