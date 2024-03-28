01350e6ae1397dcb61c30174cc2dc2e135850c7d
kebin20 <ktanzyl@hotmail.com>
2024-03-29 07:53:22 +0900

    feat: created basic component for invoice details

---
+++ src/components/InvoiceDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <a href="">Go back</a>
+  <BaseCard>
+    <h1>Status</h1>
+    <InvoiceStatus />
+  </BaseCard>
+  <section>
+    <BaseCard>
+      <header class="client-container d-flex justify-content-between mb-3">
+        <p class="font-weight-bold">#{{ invoice.id }}</p>
+        <p>{{ invoice.project }}</p>
+      </header>
+      <p>{{ invoice.address }}</p>
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+import InvoiceStatus from './UI/InvoiceStatus.vue'
+</script>
