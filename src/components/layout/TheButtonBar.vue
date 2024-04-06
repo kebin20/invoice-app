be884c29aa84bb7dd55420bef6ea3c50b33b8dae
kebin20 <ktanzyl@hotmail.com>
2024-04-06 13:06:17 +0900

    feat: created button bar component

---
+++ src/components/layout/TheButtonBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <button>Edit</button>
+    <button>Delete</button>
+    <button>Mark as Paid</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TheButtonBar'
+}
+</script>
