e5a29a0aa7da85372472ba725b671a680280fe74
kebin20 <ktanzyl@hotmail.com>
2024-04-24 08:05:32 +0900

    chore: changed utility class names

--- src/pages/InvoiceDetail.vue
+++ src/pages/InvoiceDetail.vue
@@ -7,7 +7,7 @@
   <section class="mx-3">
     <BaseCard className="flex-column">
       <header class="d-flex flex-column">
-        <h2 class="font-weight-bold body-variant">#XM9141</h2>
+        <h2 class="fw-bold fs-base">#XM9141</h2>
         <p>Graphic Design</p>
       </header>
       <main>
@@ -19,45 +19,45 @@
           <div>
             <div>
               <p>Invoice Date</p>
-              <p class="font-weight-bold color-dark-black">21 Aug 2021</p>
+              <p class="fw-bold color-dark-black">21 Aug 2021</p>
             </div>
             <div>
               <p>Payment Due</p>
-              <p class="font-weight-bold color-dark-black">20 Sep 2021</p>
+              <p class="fw-bold color-dark-black">20 Sep 2021</p>
             </div>
           </div>
           <div>
             <p>Bill To</p>
-            <p class="font-weight-bold color-dark-black">Alex Grim</p>
+            <p class="fw-bold color-dark-black">Alex Grim</p>
             <p class="lh-base">84 Church Way <br />Bradford <br />BD1 9PB<br />United Kingdom</p>
           </div>
         </div>
         <div>
           <p>Sent to</p>
-          <p class="font-weight-bold color-dark-black">alexgrim@mail.com</p>
+          <p class="fw-bold color-dark-black">alexgrim@mail.com</p>
         </div>
       </main>
       <template v-slot:footer>
         <div class="rounded-top rounded-8 mx-2 mt-4 d-flex flex-column bg-color-gray-lighter">
           <div class="d-flex align-items-center justify-content-between gap-5 px-4 pt-4">
             <div>
-              <p class="font-weight-bold color-dark-black">Banner Design</p>
+              <p class="fw-bold color-dark-black">Banner Design</p>
               <p>1 x $ 156.00</p>
             </div>
-            <p class="font-weight-bold color-dark-black">$156.00</p>
+            <p class="fw-bold color-dark-black">$156.00</p>
           </div>
           <div class="d-flex align-items-center justify-content-between gap-5 px-4">
             <div>
-              <p class="font-weight-bold color-dark-black">Email Design</p>
+              <p class="fw-bold color-dark-black">Email Design</p>
               <p>2 x $ 200.00</p>
             </div>
-            <p class="font-weight-bold color-dark-black">$400.00</p>
+            <p class="fw-bold color-dark-black">$400.00</p>
           </div>
           <div
             class="d-flex justify-content-between align-items-center w-100 gap-5 bg-color-black rounded-bottom rounded-8 p-3 py-4"
           >
             <p class="text-white mb-0">Grand Total</p>
-            <p class="text-white heading-m font-weight-bold mb-0">$556.00</p>
+            <p class="text-white heading-m fw-bold mb-0">$556.00</p>
           </div>
         </div>
       </template>
